62b5f59150c718b8ae4ea6aefdbefe8e36782342
caresam <carmen.424242@gmail.com>
2021-01-24 11:45:32 +0100

    Primer commit

---
+++ prueba.py
@@ -0,0 +1,26 @@
+file = open ("hola.txt","r+")
+
+lineas = file.readlines()
+print (lineas)
+
+ultima_linea = int(len(lineas)) - 1
+
+for i in lineas:
+    #output=len(i)-1
+        print ("{}{}".format(i[0],len(i)-1))
+    #print (i[lineas],len(i))
+ 
+a = lineas [-1]
+print (lineas[-1])
+print (lineas[0])
+print (lineas[1])
+print ("{}{}".format(a[0],len(i)))
+print (ultima_linea)
+
+file.close()
+
+
+           
+#frase =[]
+#frase = input ("Escribe algo: ")
+#print("Primera letra", frase [0], "Longitud: ", len(frase))
